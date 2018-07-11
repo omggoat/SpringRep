3884104927dae8439381e423d2c59e3d3389e5ff
omggoat <eastsun_bank@sina.com>
2018-07-11 21:14:59 +0800

    RequestMapping从方法上转移到类上

--- src/main/java/cn/shil/controller/HelloController.java
+++ src/main/java/cn/shil/controller/HelloController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String helloMvc(){
         return "success";
     }
