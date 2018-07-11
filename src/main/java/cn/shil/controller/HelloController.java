0a10ae78c1aea8266bd81469530fb8e27fe2123f
omggoat <eastsun_bank@sina.com>
2018-07-11 21:20:27 +0800

    新增controller类注释

--- src/main/java/cn/shil/controller/HelloController.java
+++ src/main/java/cn/shil/controller/HelloController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * 当控制器在类级别上添加@RequestMapping注解时，这个注解会应用到
+ * 控制器的所有处理器方法上。处理器方法上的@RequestMapping注
+ * 解会对类级别上的@RequestMapping的声明进行补充
+ */
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
